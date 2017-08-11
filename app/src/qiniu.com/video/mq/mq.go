9192c95a15ba28bc6543cab56d5cc07a3621fa7e
zjykzk <zjykzk@gmail.com>
2017-08-11 17:56:29 +0800

    using body to store use message

--- app/src/qiniu.com/video/mq/mq.go
+++ app/src/qiniu.com/video/mq/mq.go
@@ -16,77 +16,43 @@ type MQ interface {
 	Open() error
 	Close() error
 	Put(topic string, val ...Message) error
-	Get(topic string, from, count int, messages []Message) (int, error)
+	Get(topic string, from, count uint, messages *[]Message) (uint, error)
 }
 
 // Message represent the value stored in the MQ
 type Message interface {
 	Encode() []byte
 	Decode([]byte)
-
-	SetID(id []byte)
-	ID() []byte
-	SetCreatedAt(uint64)
-	CreatedAt() uint64
-	SetStatus(int)
-	Status()
 }
 
 // BaseMessage message common data
-type BaseMessage struct {
+type message struct {
 	id        []byte
 	createdAt uint64
 	status    uint16
-}
-
-// SetID store the message id
-func (bm *BaseMessage) SetID(id []byte) {
-	bm.id = id
-}
-
-// ID return message id
-func (bm *BaseMessage) ID() []byte {
-	return bm.id
-}
-
-// SetCreatedAt set create time
-func (bm *BaseMessage) SetCreatedAt(time uint64) {
-	bm.createdAt = time
-}
-
-// CreatedAt return message created time
-func (bm *BaseMessage) CreatedAt() uint64 {
-	return bm.createdAt
-}
-
-// SetStatus set message status
-func (bm *BaseMessage) SetStatus(status uint16) {
-	bm.status = status
-}
-
-// Status return message status
-func (bm *BaseMessage) Status() uint16 {
-	return bm.status
+	body      []byte
 }
 
 var endian = binary.BigEndian
 
 // Encode the base message to bytes
-func (bm *BaseMessage) Encode() []byte {
-	idLen := len(bm.id)
-	bytes := make([]byte, 2+idLen+8+2)
+func (m *message) Encode() []byte {
+	idLen, bodyLen := len(m.id), len(m.body)
+	bytes := make([]byte, 2+idLen+8+2+bodyLen)
 	endian.PutUint16(bytes, uint16(idLen))
-	copy(bytes[2:], bm.id)
-	endian.PutUint64(bytes[2+idLen:], bm.createdAt)
-	endian.PutUint16(bytes[2+idLen+8:], bm.status)
+	copy(bytes[2:], m.id)
+	endian.PutUint64(bytes[2+idLen:], m.createdAt)
+	endian.PutUint16(bytes[2+idLen+8:], m.status)
+	copy(bytes[2+idLen+8+2:], m.body)
 
 	return bytes
 }
 
 // Decode the raw bytes to `BaseMessage`
-func (bm *BaseMessage) Decode(bytes []byte) {
+func (m *message) Decode(bytes []byte) {
 	idLen := endian.Uint16(bytes)
-	bm.id = bytes[2 : 2+idLen]
-	bm.createdAt = endian.Uint64(bytes[2+idLen:])
-	bm.status = endian.Uint16(bytes[2+idLen+8:])
+	m.id = bytes[2 : 2+idLen]
+	m.createdAt = endian.Uint64(bytes[2+idLen:])
+	m.status = endian.Uint16(bytes[2+idLen+8:])
+	m.body = bytes[2+idLen+8+2:]
 }
--- app/src/qiniu.com/video/mq/mq_test.go
+++ app/src/qiniu.com/video/mq/mq_test.go
@@ -7,17 +7,19 @@ import (
 )
 
 func TestBaseMessage(t *testing.T) {
-	m := BaseMessage{
+	m := message{
 		id:        []byte("id"),
 		createdAt: 9999,
 		status:    StatusDeleted,
+		body:      []byte("body"),
 	}
 
-	md := BaseMessage{}
+	md := message{}
 
 	md.Decode(m.Encode())
 
 	assert.Equal(t, m.id, md.id)
 	assert.Equal(t, m.createdAt, md.createdAt)
 	assert.Equal(t, uint16(StatusDeleted), md.status)
+	assert.Equal(t, m.body, md.body)
 }
