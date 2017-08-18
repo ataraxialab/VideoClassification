6f9fe6291e293bccbf811df02be1566ea87673b3
zjykzk <zjykzk@gmail.com>
2017-08-18 13:52:04 +0800

    build

---
+++ app/src/qiniu.com/video/builder/builder_test.go
@@ -0,0 +1,95 @@
+package builder
+
+import (
+	"os"
+	"testing"
+
+	"qiniu.com/video/flow"
+	"qiniu.com/video/frame"
+	"qiniu.com/video/logger"
+	"qiniu.com/video/pattern"
+	"qiniu.com/video/target"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func newFile(t *testing.T, filename, data string) string {
+	f, err := os.Create(filename)
+	f.WriteString(data)
+	f.Close()
+	assert.Nil(t, err)
+	return filename
+}
+
+func TestValid(t *testing.T) {
+	assert.NotNil(t, Valid())
+	defer func() {
+		os.Remove(trainLabelFile)
+		os.Remove(valLabelFile)
+	}()
+	videoRoot = "."
+	assert.NotNil(t, Valid())
+
+	trainLabelFile = newFile(t, "trainLabelFile", "abc,123")
+	assert.NotNil(t, Valid())
+
+	valLabelFile = newFile(t, "valLabelFile", "ab1c,123")
+	assert.Nil(t, Valid())
+
+	cmd := &cmdRandom{
+		logger: logger.Std,
+	}
+	assert.Nil(t, cmd.Init())
+	assert.Equal(t, 2, len(cmd.videoName2Label))
+}
+
+func TestLoadLabel(t *testing.T) {
+	f1 := newFile(t, "invalid_label", "abc,123\n111")
+	_, err := loadLabel(f1)
+	assert.NotNil(t, err)
+	os.Remove(f1)
+	f1 = newFile(t, "invalid_label", "abc,123\n111, abc")
+	_, err = loadLabel(f1)
+	assert.NotNil(t, err)
+	_, err = loadLabel(f1)
+	assert.NotNil(t, err)
+	os.Remove(f1)
+}
+
+func TestBuilder(t *testing.T) {
+	videoRoot = "."
+	trainLabelFile = newFile(t, "trainLabelFile", "p,2\ntrainLabelFile,1\ncmd,123\n\nfactory,111")
+	valLabelFile = newFile(t, "valLabelFile", "valLabelFile,2\nbuilder,123\nbulder_test,1")
+	program = newFile(t, "./program", "#!/bin/env bash\nmkdir -p $4 && touch $4/frame && touch $4/flow")
+	os.Chmod(program, os.ModePerm)
+	defer func() {
+		os.Remove(trainLabelFile)
+		os.Remove(valLabelFile)
+		os.Remove(program)
+		os.RemoveAll(outputRoot)
+	}()
+
+	cmd := GetBuilder(Cmd, target.Frame, pattern.Random)
+	assert.Nil(t, cmd.Init())
+	ret, err := cmd.Build(Params{
+		Count:  10,
+		Offset: 0.2,
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(ret))
+	_, ok := ret[0].(frame.Frame)
+	assert.True(t, ok)
+
+	os.RemoveAll(outputRoot)
+	cmd = GetBuilder(Cmd, target.Flow, pattern.Random)
+	assert.Nil(t, cmd.Init())
+	assert.Nil(t, err)
+	ret, err = cmd.Build(Params{
+		Count:  100,
+		Offset: 0.12,
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(ret))
+	_, ok = ret[0].(flow.Flow)
+	assert.True(t, ok)
+}
--- app/src/qiniu.com/video/builder/cmd.go
+++ app/src/qiniu.com/video/builder/cmd.go
@@ -1,11 +1,15 @@
 package builder
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
@@ -18,24 +22,39 @@ import (
 
 const (
 	// Cmd call external command to build data
-	Cmd     Implement = "cmd"
-	program string    = "./export_frames"
+	Cmd Implement = "cmd"
+)
+
+var (
+	program = "./export_frames"
 )
 
 type cmdRandom struct {
-	name       string
-	logger     *logger.Logger
-	buildLabel func(string) (interface{}, error)
+	name            string
+	logger          *logger.Logger
+	buildLabel      func(string, int) (interface{}, error)
+	videos          []string
+	videoName2Label map[string]int
 }
 
 func createOutputDir(prefix string) (string, error) {
-	d := path.Join(outputRoot, prefix, time.Now().Format("20060102-150405."))
+	d := path.Join(outputRoot, prefix, time.Now().Format("20060102-150405.000"))
 	return d, os.MkdirAll(d, os.ModePerm)
 }
 
-func selectVideo() string {
-	// TODO
-	return "test.mp4"
+func (f *cmdRandom) selectVideo() string {
+	return f.videos[rand.Intn(len(f.videos))]
+}
+
+// interpret label name from video path
+func labelName(video string) string {
+	basename := path.Base(video)
+	dotIndex := strings.IndexByte(basename, '.')
+	if dotIndex < 0 {
+		return basename
+	}
+
+	return basename[:dotIndex]
 }
 
 func (f *cmdRandom) Build(params interface{}) ([]interface{}, error) {
@@ -51,8 +70,15 @@ func (f *cmdRandom) Build(params interface{}) ([]interface{}, error) {
 		return nil, fmt.Errorf("create output dir failed:%s", e.Error())
 	}
 
-	arg := fmt.Sprintf("-interval 10 -i %s -o %s -postfix jpg -c %d -ss %f -s 256x256",
-		selectVideo(), output, p.Count, p.Offset)
+	video := f.selectVideo()
+	labelName := labelName(video)
+	label, exist := f.videoName2Label[labelName]
+	if !exist {
+		return nil, fmt.Errorf("not exist of label:%s, video:%s", labelName, video)
+	}
+
+	arg := fmt.Sprintf("-i %s -o %s -postfix jpg -c %d -ss %f -s 256x256",
+		video, output, p.Count, p.Offset)
 
 	f.logger.Errorf("run cmd:%s %s", program, arg)
 	cmd := exec.Command(program, strings.Split(arg, " ")...)
@@ -65,13 +91,13 @@ func (f *cmdRandom) Build(params interface{}) ([]interface{}, error) {
 		return nil, e
 	}
 
-	return f.buildLabels(f.getFiles(output)), nil
+	return f.buildLabels(f.getFiles(output), label), nil
 }
 
-func (f *cmdRandom) buildLabels(files []string) []interface{} {
+func (f *cmdRandom) buildLabels(files []string, label int) []interface{} {
 	ret := make([]interface{}, 0, len(files))
 	for _, file := range files {
-		l, err := f.buildLabel(file)
+		l, err := f.buildLabel(file, label)
 		if err != nil {
 			f.logger.Errorf("build label error:%v", err)
 			continue
@@ -81,18 +107,16 @@ func (f *cmdRandom) buildLabels(files []string) []interface{} {
 	return ret
 }
 
-func buildFrame(file string) (interface{}, error) {
-	// FIXME
+func buildFrame(file string, label int) (interface{}, error) {
 	return frame.Frame{
-		Label:     float32(999),
+		Label:     float32(label),
 		ImagePath: file,
 	}, nil
 }
 
-func buildFlow(file string) (interface{}, error) {
-	// FIXME
+func buildFlow(file string, label int) (interface{}, error) {
 	return flow.Flow{
-		Label:     float32(999),
+		Label:     float32(label),
 		ImagePath: file,
 	}, nil
 }
@@ -121,6 +145,92 @@ func (f *cmdRandom) Clean(result interface{}) error {
 		return os.Remove(r.ImagePath)
 	}
 	return nil
+}
+
+func (f *cmdRandom) Init() error {
+	if err := Valid(); err != nil {
+		return err
+	}
+
+	if f.videos == nil {
+		videos := make([]string, 0, 1024)
+		filepath.Walk(videoRoot,
+			func(p string, info os.FileInfo, err error) error {
+				if err != nil {
+					f.logger.Errorf("error :%v", err)
+					return nil // IGNORE
+				}
+				if videoRoot == p {
+					return nil
+				}
+				videos = append(videos, p)
+				return nil
+			})
+		f.videos = videos
+	}
+
+	videoCount := len(f.videos)
+	if len(f.videos) == 0 {
+		return fmt.Errorf("no videos under:%s", videoRoot)
+	}
+
+	f.logger.Infof("video count:%d", videoCount)
+
+	m, err := loadLabels(trainLabelFile, valLabelFile)
+	if err != nil {
+		f.logger.Errorf("load labels error:%v", err)
+		return fmt.Errorf("load labels error:%s", err.Error())
+	}
+
+	f.logger.Infof("label count:%d", len(m))
+	f.videoName2Label = m
+	return nil
+}
+
+func loadLabel(labelFile string) (map[string]int, error) {
+	content, err := ioutil.ReadFile(labelFile)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := bytes.Split(content, []byte("\n"))
+	m := make(map[string]int, len(lines))
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		elems := bytes.Split(line, []byte(","))
+		if len(elems) < 2 {
+			return nil, fmt.Errorf("bad label data:%s", line)
+		}
+		m[string(elems[0])], err = strconv.Atoi(string(elems[1]))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return m, nil
+}
+
+func loadLabels(labelfiles ...string) (labels map[string]int, err error) {
+	for _, f := range labelfiles {
+		m, err := loadLabel(f)
+		if err != nil {
+			return nil, err
+		}
+
+		if labels == nil {
+			labels = m
+			continue
+		}
+
+		for k, v := range m {
+			labels[k] = v
+		}
+	}
+
+	return labels, nil
 }
 
 type cmdFrameRandom struct {
